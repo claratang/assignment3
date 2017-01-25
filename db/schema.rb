c0f4354128c9631cdec8a44c23af7d72d4da2d9f
Clara Tang <claragtang@gmail.com>
2017-01-25 20:30:10 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118011426) do
+ActiveRecord::Schema.define(version: 20170125201332) do
 
   create_table "home_states", force: :cascade do |t|
     t.string   "name"
     t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
